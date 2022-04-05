7ea9df74b2a174d55f41d153e1fa070f25aed457
Archanagowda23 <archanar249@gmail.com>
2022-04-05 17:43:55 +0530

    Added: created contact in address book

--- src/com/bridgelabz/algorithmprograms/AddressBook.java
+++ src/com/bridgelabz/algorithmprograms/AddressBook.java
@@ -1,8 +1,36 @@
 package com.bridgelabz.algorithmprograms;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddressBook {
+
+    static Scanner scanner = new Scanner(System.in);
+    static ArrayList<Person> addressBook = new ArrayList<Person>();
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Address Book Problem");
+        System.out.println("Enter a first name:");
+        String firstName=scanner.next();
+        System.out.println("Enter a last name:");
+        String lastName=scanner.next();
+        System.out.println("Enter a Address:");
+        String address=scanner.next();
+        System.out.println("Enter a City name:");
+        String city=scanner.next();
+        System.out.println("Enter a state:");
+        String state=scanner.next();
+        System.out.println("Enter a email:");
+        String email=scanner.next();
+        System.out.println("Enter a zip code:");
+        int  zip=scanner.nextInt();
+        System.out.println("Enter a phone number:");
+        long phoneNumber=scanner.nextLong();
+
+        Person details1=new Person(firstName, lastName, address,  city, state, email,  zip, phoneNumber);
+        addressBook.add(details1);
+        System.out.print(addressBook);
+        System.out.println("succesfully created contacts");
     }
 }
+
